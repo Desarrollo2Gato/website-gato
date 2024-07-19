565410cc2aba149d77f09bbcc9c84e55e7bc2222
DevDanielaCN <dani.cn@outlook.es>
2024-07-18 23:27:01 -0500

    robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,17 +1,18 @@
 import type { MetadataRoute } from 'next'
- 
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: '*',
-        allow: ['/'],
-        disallow:['admin', '/private/'],
+        allow: '/',
+        disallow: '/private/',
       },
-     /*  {
+      /* {
         userAgent: ['Applebot', 'Bingbot'],
         disallow: ['/'],
       }, */
+
     ],
     sitemap: 'https://www.gato.pe/sitemap.xml',
   }
