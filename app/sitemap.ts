85d4079f4925a6a5aee11db08e3295853c8a04bc
DevDanielaCN <dani.cn@outlook.es>
2024-07-21 19:49:17 -0500

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -52,36 +52,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     ];
 
 
-    const allUrls: MetadataRoute.Sitemap = [
+    return [
         ...staticUrls,
         ...projectsDetails,
         ...blogCategories,
         ...blogPosts,
         ...jobVacancies,
     ];
-    return allUrls;
 }
 
 
-/* export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://acme.com',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: 'https://acme.com/about',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://acme.com/blog',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-  ]
-} */
